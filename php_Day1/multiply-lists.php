70b3638a864674509d297f63aa83efd6d7e8e9ee
Ankit Arora <ankit2102sigma>
2023-02-27 16:02:21 +0530

    commit

---
+++ php_Day1/multiply-lists.php
@@ -0,0 +1,12 @@
+<?php
+function multiply_two_lists($x, $y)
+  {
+    $a = explode(' ',trim($x));
+    $b = explode(' ',trim($y));
+    foreach($a as $key=>$value){
+        $output[$key] = $a[$key]*$b[$key];
+    }
+    return implode(' ',$output);
+}
+echo multiply_two_lists(("20 12 5"), ("1 0 3"))."\n";
+?>
