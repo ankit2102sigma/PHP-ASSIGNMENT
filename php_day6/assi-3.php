f00b8bb5e07f840d056a662fb1a2df5ec1b75c8e
Ankit Arora <ankit2102sigma>
2023-03-01 11:51:13 +0530

    commit

--- php_day6/assi-3.php
+++ php_day6/assi-3.php
@@ -1,7 +1,7 @@
 <?php
   
-$json_data = '{"A": 2102, "B": 2102, "C": 2023, "D":2024, "E":2025}';
+$arr = array("A"=> 2102, "B"=> 2102, "C"=> 2023, "D"=>2024, "E"=>2025);
 
-var_dump(json_decode($json_data));
+var_dump(json_encode($arr));
 echo "\n";
 ?>
